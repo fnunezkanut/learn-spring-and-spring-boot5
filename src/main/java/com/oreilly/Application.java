beb8e96245483580a0ace0bfbacfebeeb7e04b16
fnunezkanut <fnunezkanut@users.noreply.github.com>
2017-02-14 14:24:18 +0000

    60

--- src/main/java/com/oreilly/Application.java
+++ src/main/java/com/oreilly/Application.java
@@ -3,6 +3,7 @@ package com.oreilly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -17,5 +18,12 @@ public class Application {
 	public String viewResolver(){
 
 		return "viewresolver";
+	}
+
+	@RequestMapping("/mustache")
+	public String mustache(Model model){
+
+		model.addAttribute("company","Oreilly");
+		return "template";
 	}
 }
